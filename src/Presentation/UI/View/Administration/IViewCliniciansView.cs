56a39cb621388c2f32f791a4d6a8b130a059565f
drielnox <bl4z3r@gmail.com>
2016-01-22 01:39:26 -0300

    In the last commit, this file are not commited.

---
+++ src/Presentation/UI/View/Administration/IViewCliniciansView.cs
@@ -0,0 +1,16 @@
+﻿
+using CIS.Presentation.Model.Clinicians;
+using CIS.Presentation.Model.Common;
+using System.Collections.Generic;
+
+namespace CIS.Presentation.UI.Contracts.Administration
+{
+    public interface IViewCliniciansView
+    {
+        void LoadClinicians(IEnumerable<ClinicListViewModel> clinicians);
+
+        EditClinicViewModel GetClinic();
+
+        void ShowEditClinicForm(EditClinicViewModel clinic);
+    }
+}
---
+++ src/Presenter/Administration/ViewCliniciansPresenter.cs
@@ -0,0 +1,31 @@
+﻿
+using CIS.Application.BusinessComponents;
+using CIS.Presentation.Model.Clinicians;
+using CIS.Presentation.UI.Contracts.Administration;
+
+namespace CIS.Presentation.Logic.Presenter.Administration
+{
+    public class ViewCliniciansPresenter
+    {
+        private IViewCliniciansView _view;
+        private ClinicianBusinessLogic _clinicianLogic;
+
+        public ViewCliniciansPresenter(IViewCliniciansView view)
+        {
+            _view = view;
+            _clinicianLogic = new ClinicianBusinessLogic();
+        }
+
+        public void LoadClinics()
+        {
+            var clinicians = _clinicianLogic.GetClinicians();
+            _view.LoadClinicians(clinicians);
+        }
+
+        public void ShowEditClinic()
+        {
+            EditClinicViewModel clinic = _view.GetClinic();
+            _view.ShowEditClinicForm(clinic);
+        }
+    }
+}
