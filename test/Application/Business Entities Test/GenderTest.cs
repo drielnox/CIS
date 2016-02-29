778a2028efe38ed80a10ec5ea9315db2ee22a112
drielnox <bl4z3r@gmail.com>
2016-02-28 22:40:17 -0300

    Added test projects

---
+++ test/Application/Business Entities Test/GenderTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CIS.Application.Entities;
+
+namespace CIS.Application.BusinessEntities.Test
+{
+    [TestClass]
+    public class GenderTest
+    {
+        #region Constructors
+
+        [TestMethod]
+        public void ConstructNewGenderWithMandatoryParameters()
+        {
+            var gender = new Gender(1, "Male");
+
+            Assert.IsNotNull(gender);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructNewGenderWithInvalidIdentifierThrowsException()
+        {
+            var gender = new Gender(-1, "Male");
+
+            Assert.Fail("Its no posible create a new gender with negative identifier.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructNewGenderWithEmptyDescriptionThrowsException()
+        {
+            var gender = new Gender(1, string.Empty);
+
+            Assert.Fail("Its no posible create a new gender with empty description.");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CostructNewGenderWithNullDescriptionThrowsException()
+        {
+            var gender = new Gender(1, null);
+
+            Assert.Fail("Its no posible create a new gender with empty description.");
+        }
+
+        #endregion
+    }
+}
---
+++ test/Presentation/Presentation Models Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Presentation_Models_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
