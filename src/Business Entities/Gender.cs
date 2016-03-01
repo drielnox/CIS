1647bfe3bbb11b4aab254cbae7eb25d38a6d08a8
drielnox <bl4z3r@gmail.com>
2016-03-01 01:07:25 -0300

    test work now.

--- src/Business Entities/Gender.cs
+++ src/Business Entities/Gender.cs
@@ -1,10 +1,15 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 
 namespace CIS.Application.Entities
 {
     [DataContract]
     public class Gender
     {
+        private int _identifier;
+
+        private string _description;
+
         [DataMember]
         public static readonly Gender MALE = new Gender(1, "Male");
 
@@ -12,10 +17,45 @@ namespace CIS.Application.Entities
         public static readonly Gender FEMALE = new Gender(2, "Female");
 
         [DataMember]
-        public int Identifier { get; protected set; }
+        public int Identifier
+        {
+            get
+            {
+                return _identifier;
+            }
+            protected set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                _identifier = value;
+            }
+        }
 
         [DataMember]
-        public string Description { get; protected set; }
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            protected set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException();
+                }
+
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException();
+                }
+
+                _description = value;
+            }
+        }
 
         public Gender(int identifier, string description)
         {
